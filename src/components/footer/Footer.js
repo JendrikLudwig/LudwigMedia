075f94567c7b669b1897bba6e0d6afaa90957488
LudwigATsl <46196808+LudwigATsl@users.noreply.github.com>
2020-04-22 06:55:14 +0200

    Visuals Finished. Animation and mobile Nav Missing.

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./footer.css"
 
+import twitterIco from "../../img/twitter.svg";
+import instaIco from "../../img/instagram.svg";
+import tubeIco from "../../img/youtube.svg";
+
 
 function Footer(){
   return(
-    <div className="footer">Footer</div>
+    <div className="footer">
+      <div className="footerNav">
+        <a>Contact</a>
+        <a>Imprint</a>
+      </div>
+      <div className="footerSocial">
+        <img src={twitterIco}/>
+        <img src={instaIco}/>
+        <img src={tubeIco}/>
+      </div>
+    </div>
   )
 }
 
